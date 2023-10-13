7d196b138aa74bea90e91d0ec4e8fca77b8c29d5
SANGWOO PARK <cyberprophet@hanmail.net>
2023-10-13 10:59:07 +0900

    change the attributes

---
+++ BlazorApp1/Pages/_Host.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Pages
+{
+    [ValidateAntiForgeryToken, ResponseCache(Duration = 0x100, Location = ResponseCacheLocation.Client, NoStore = false)]
+    public partial class HostModel : PageModel
+    {
+        public void OnGet()
+        {
+            logger.LogInformation("Host: { }, Path: { }, RemoteIpAddress: { }",
+                                  HttpContext.Request.Host,
+                                  HttpContext.Request.Path,
+                                  HttpContext.Connection.RemoteIpAddress);
+        }
+        public HostModel(ILogger<HostModel> logger)
+        {
+            this.logger = logger;
+        }
+        readonly ILogger<HostModel> logger;
+    }
+}
